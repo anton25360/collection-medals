9894b623acc89c961df8e1e88cb5e612c85c6378
Anton Webb <anton25360@gmail.com>
2019-09-23 15:58:46 +0100

    Displayed data correcly from db. Added emojis.

--- index.php
+++ index.php
@@ -1,16 +1,39 @@
 <?php
 
-?>
+$db = new PDO('mysql:host=db;dbname=collection','root','password');
+$db -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+$query = $db -> query('SELECT `event`,`year`,`place` FROM `events`');
+$events = $query ->fetchAll();
+
+foreach ($events as $eventTable) {
+    $event = $eventTable['event'];
+    $year = $eventTable['year'];
+    $place = $eventTable['place'];
+
+    $message='';
+    if ($place == '1st'){
+        $message = ' 🥇';
+    } elseif ($place == '2nd') {
+        $message = ' 🥈';
+    } elseif ($place == '3rd') {
+        $message = ' 🥉';
+    } else {
+        $message = ' 🤮';
+    }
+
+    echo '<p> Event: ' . $event . ', '. $year . '. You came ' . $place . $message . '</p>';
+}
+
+?>
 <!DOCTYPE html>
 <head>
     <title>title</title> <!-- tab title -->
     <link href="normalize.css" rel="stylesheet" type="text/css"> <!-- normalize file -->
     <link href="style.css" rel="stylesheet" type="text/css"> <!-- css file -->
     <link href="assets/webfonts/stylesheet.css" rel="stylesheet" type="text/css"> <!-- font file -->
-    <meta name="viewport" content="initial-scale=1"> <!-- responsive meta tag -->
+
 </head>
 
 <body>
-<h1>Boilerplate</h1>
 </body>
